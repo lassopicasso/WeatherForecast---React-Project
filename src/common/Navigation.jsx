ba96d2e56e2b4dbb685f2a4b29381634de076eb0
Lars <lars.walderhaug@hotmail.com>
2022-08-14 14:22:27 +0200

    removed unused useeffect

--- src/common/Navigation.jsx
+++ src/common/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
