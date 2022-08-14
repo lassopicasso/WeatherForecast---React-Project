21cbb71390d2470d280ff6f2602dee62992379f5
Lars <lars.walderhaug@hotmail.com>
2022-08-14 14:15:22 +0200

    nav corrections

--- src/common/Navigation.jsx
+++ src/common/Navigation.jsx
@@ -10,7 +10,7 @@ function Navigation() {
       <Container>
         <Navbar.Brand className="text-uppercase">Weatherish</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav ">
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={NavLink} className="text-uppercase" to="/">
               Weather
