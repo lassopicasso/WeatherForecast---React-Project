d62659d66cc15d5ba755accccd201fef1fe529e9
Lars <lars.walderhaug@hotmail.com>
2022-08-14 14:20:02 +0200

    nav hide when clicked on link - mobile

--- src/common/Navigation.jsx
+++ src/common/Navigation.jsx
@@ -5,20 +5,21 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" expanded={expanded}>
       <Container>
         <Navbar.Brand className="text-uppercase">Weatherish</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} className="text-uppercase" to="/">
+            <Nav.Link as={NavLink} className="text-uppercase" to="/" onClick={() => setExpanded(false)}>
               Weather
             </Nav.Link>
-            <Nav.Link as={NavLink} className="text-uppercase" to="/favorites">
+            <Nav.Link as={NavLink} className="text-uppercase" to="/favorites" onClick={() => setExpanded(false)}>
               Favorites
             </Nav.Link>
-            <Nav.Link as={NavLink} className="text-uppercase" to="/contact">
+            <Nav.Link as={NavLink} className="text-uppercase" to="/contact" onClick={() => setExpanded(false)}>
               Contact
             </Nav.Link>
           </Nav>
